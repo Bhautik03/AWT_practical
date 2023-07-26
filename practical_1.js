8c86d8a56399ffd1fd1ca201988684bdf49b9988
Bhautik03 <126908149+Bhautik03@users.noreply.github.com>
2023-07-26 22:24:59 +0530

    Add files via upload

---
+++ practical_1.js
@@ -0,0 +1,15 @@
+const a1 = "Bhautik";
+const a2 = 20;
+const a3 = true;
+
+console.log("String =>", a1);
+console.log("Number =>", a2);
+console.log("Boolean =>", a3);
+
+function add(n1, n2) {
+    return n1 + n2;
+}
+const n1 = 5;
+const n2 = 10;
+const sum = add(n1, n2);
+console.log("Sum is =>", sum);
